2a492b6f9aec8d46040058da09ef8783cfbd551e
flupe <lucas@escot.me>
2017-01-28 00:38:21 +0100

    improved drawing proc

--- src/dessine.ml
+++ src/dessine.ml
@@ -1,27 +1,34 @@
 open Visited
 open Graph.Pack
 
-(* WARNING : working but very very bad *)
+(* a variant of the VisitedNodes module *)
+module Memory = struct
+  type 'a t = ('a tree * Digraph.V.t) list ref
 
-module VisitedNodes = MakeVisitor(struct
-    type 'a t = 'a tree * Digraph.V.t
-    let eq (t1, _) (t2, _) = t1 == t2
-end)
+  let create () = ref []
+
+  (* val lookup : 'a t -> 'a tree -> Digraph.V.t option *)
+  let lookup storage tree =
+    let rec search = function
+      | (t, node) :: q -> if t == tree then Some node else search q
+      | _ -> None
+    in
+    search !storage
+
+  let add storage tree vertex =
+    storage := (tree, vertex) :: !storage
+
+end
 
 let affiche tree =
   let graph = Digraph.create () in
+  let storage = Memory.create () in
   let count = ref 0 in
-  let storage = VisitedNodes.create () in
 
   let succ () =
-    let v  = !count in begin
+    let v = !count in begin
       incr count; v
     end
-  in
-
-  let rec find_saved_node t = function
-    | (a, n) :: q -> if a == t then n else find_saved_node t q
-    | _ -> failwith "ok"
   in
 
   let rec build = function
@@ -32,17 +39,18 @@ let affiche tree =
       end
 
     | Node(x, g, d) as node ->
-      let node_vertex = Digraph.V.create (succ ()) in
-      if VisitedNodes.lookup (node, node_vertex) storage then
-        find_saved_node node (VisitedNodes.tolist storage)
-      else
-      let left_child = build g in
-      let right_child = build d in begin
-        Digraph.add_vertex graph node_vertex;
-        Digraph.add_edge graph node_vertex left_child;
-        Digraph.add_edge graph node_vertex right_child;
-        node_vertex
-      end
+      match Memory.lookup storage node with
+      | Some vertex -> vertex
+      | None ->
+        let vertex = Digraph.V.create (succ ()) in
+        let left_child = build g in
+        let right_child = build d in begin
+          Digraph.add_vertex graph vertex;
+          Digraph.add_edge graph vertex left_child;
+          Digraph.add_edge graph vertex right_child;
+          Memory.add storage node vertex;
+          vertex
+        end
 
   in
   let _ = build tree in
